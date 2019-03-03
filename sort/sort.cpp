8e15bbb5e2d46111a6894d688331678080b76b11
ticks <xiehuiwu@whu.edu.cn>
2019-03-03 20:12:51 +0800

    完成插入排序，新增归并排序

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -1,3 +1,21 @@
 #include "sort.h"
 
-void InsertSort(int* ar, int n) {}
+void InsertSort(int* ar, int n)
+{
+	int key;
+	for (int i = 1; i < n; i++)
+	{
+		key = ar[i];
+		int j;
+		for (j = i - 1; j >= 0; j--)
+		{
+			if (key < ar[j])
+				ar[j + 1] = ar[j];
+			else
+				break;
+		}
+		ar[j + 1] = key;
+	}
+}
+
+void MergeSort(int* ar, int n) {}
--- sort/test.cpp
+++ sort/test.cpp
@@ -1,32 +1,54 @@
 #include "sort.h"
 #include <cstdlib>
+#include <cstring>
 #include <ctime>
 #include <iostream>
 
 static int Array_Init[50];
 
-enum SortMethod { Insert_Sort, Merge_Sort };
+enum SortMethod
+{
+	Insert_Sort,
+	Merge_Sort
+};
 
-enum { Sort_Success, Sort_Failure };
+enum
+{
+	Sort_Success,
+	Sort_Failure
+};
 
 static int TestSort(const int* ar, int n, SortMethod method)
 {
-	for (size_t i = 1; i < n; ++i)
+	int temp[50];
+	memcpy(temp, ar, 50 * sizeof(int));
+	switch (method)
 	{
-		if (ar[i] < ar[i - 1])
+	case Insert_Sort:
+		InsertSort(temp, 50);
+	case Merge_Sort:
+		MergeSort(temp, 50);
+	}
+	for (int i = 1; i < n; ++i)
+	{
+		if (temp[i] < temp[i - 1])
 		{
 			std::cout << method << " failed " << std::endl;
 			return Sort_Failure;
 		}
 	}
+	std::cout << method << " success" << std::endl;
 	return Sort_Success;
 }
 
 int main()
 {
 	srand(time(NULL));
-	for (size_t i = 0; i < 50; ++i) { Array_Init[i] = rand() % 100; }
-	InsertSort(Array_Init, 50);
+	for (int i = 0; i < 50; ++i)
+	{
+		Array_Init[i] = rand() % 100;
+	}
 	TestSort(Array_Init, 50, Insert_Sort);
+	TestSort(Array_Init, 50, Merge_Sort);
 	return 0;
 }
